d85d68b6a929d33d289960a0e5ac19865b2d5c6b
Ziga <ziga.zajc007@gmail.com>
2023-06-25 21:56:04 +0200

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://rabbit-hosting.com',
+  integrations: [tailwind(),sitemap()],
   trailingSlash: 'never',
   build: {
     format: 'file'
