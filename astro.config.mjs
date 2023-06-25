a6ff504ccd991155da74975957d0248a89081dda
Ziga <ziga.zajc007@gmail.com>
2023-06-25 21:42:21 +0200

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  trailingSlash: 'never'
+  trailingSlash: 'never',
+  build: {
+    format: 'file'
+  }
 });
